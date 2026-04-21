16cf8852e47bea403c395ce13a350428ee24659e
sureways739-art <sureways739@gmail.com>
2026-04-21 01:09:44 +0100

    Log MONGO_URI to console for debugging

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log("MONGO_URI:", process.env.MONGO_URI);
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
